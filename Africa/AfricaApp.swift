b81cd1b106dd794ee35e11b5ccc7c6692373ddf9
Ali Hassan <ahassan@inthekloud.com>
2021-08-05 14:15:00 +0200

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Ali Hassan on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
