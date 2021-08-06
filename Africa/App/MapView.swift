317f7215604bdc68f481d08d77c349da139d5a43
Ali Hassan <ahassan@inthekloud.com>
2021-08-07 01:52:43 +0200

    Create Custom Animated Pin on Map

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -31,16 +31,51 @@ struct MapView: View {
                 //2- Pin : New Style (static)
                 // MapMarker(coordinate: item.location, tint: .accentColor)
                 
-                // Custom Pin annotation
+                // Custom Pin annotation (animated)
                 MapAnnotation(coordinate: item.location){
-                    Image("logo")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 32, height: 32, alignment: .center)
+                    MapAnnotationView(location: item)
                 }
             }
-            
-            )
+        )
+        .overlay(
+            // Top Compass to show centered Coordinates
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 12, content: {
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48, height: 48, alignment: .center)
+                VStack (alignment: .leading,spacing: 3){
+                    HStack( content: {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    })
+                    Divider()
+                    HStack( content: {
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    })
+                }
+            })
+            .padding(.vertical,12)
+            .padding(.horizontal,16)
+            .background(Color.black)
+            .opacity(0.6)
+            .cornerRadius(8)
+            .padding()
+            ,alignment: .top
+        )
     }
 }
 
