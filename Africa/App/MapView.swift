c04594a6fd3dd146503748d462842eef3be236df
Ali Hassan <ahassan@inthekloud.com>
2021-08-07 01:04:34 +0200

    Create Map With Pins

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,10 +6,41 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70, longitudeDelta: 70)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+    }()
+    
+    let location: [NationalParkLoaction] = Bundle.main.decode("locations.json")
+    
     var body: some View {
-        Text("Map")
+        Map(
+            
+            coordinateRegion: $region,
+            annotationItems: location,
+            annotationContent: {
+                item in
+                // 1- Pin : Old Style (static)
+                // MapPin(coordinate: item.location, tint: .accentColor)
+                
+                //2- Pin : New Style (static)
+                // MapMarker(coordinate: item.location, tint: .accentColor)
+                
+                // Custom Pin annotation
+                MapAnnotation(coordinate: item.location){
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32, alignment: .center)
+                }
+            }
+            
+            )
     }
 }
 
