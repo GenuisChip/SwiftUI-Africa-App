b34be1949ea408c1b3c3182822a05ac37e2fc751
Ali Hassan <ahassan@inthekloud.com>
2021-08-11 10:45:41 +0200

    Create Photo Gallery With Grid Layout

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    @State var columnsNumber = 3.0
+    @State var selectedItem : Animal?
+    let animals : [Animal] = Bundle.main.decode("animals.json")
+    
+    
     var body: some View {
         ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, content: {
-            Text("Gallary")
+            VStack(spacing: 20){
+                
+                Image("\(selectedItem?.id ?? "lion")")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                
+                
+                Slider(value: $columnsNumber, in: 1...4, step: 1)
+                    .padding(.horizontal,24)
+                
+                LazyVGrid(
+                    columns: Array.init(repeating: GridItem(), count: Int(columnsNumber)),
+                    alignment: .center,
+                    spacing: 20,
+                    content: {
+                        ForEach(animals){item in
+                            Image(item.id)
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.white,lineWidth: 1))
+                                .onTapGesture {
+                                    selectedItem = item
+                                }
+                            
+                    }
+                })
+            }.animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
         })
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         .background(MotionAnimationView())
